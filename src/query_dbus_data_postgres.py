cea28bcb3df5d1988329b66f8029595a7f5888bf
Ethan Xuan <xuan.yiguang@gmail.com>
2014-11-12 13:45:28 -0800

    Add shape_id and route_short_name to query results
    - requires joining the trips and routes table
    - requires querying the t_range of trips and routes
    
    other changes:
     - add starttime_ms to the argument parser
     - fix bug when file does not exist

--- src/query_dbus_data_postgres.py
+++ src/query_dbus_data_postgres.py
@@ -53,23 +53,39 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Query Dbus data from postgres")
     parser.add_argument("-u", "--username", dest="username", help="Username for DB", required=True)
     parser.add_argument("-p", "--password", dest="password", help="Password for DB", required=True)
-    parser.add_argument("-d", "--days", dest="days", help="Number of days to download", default=1)
+    parser.add_argument("-d", "--days", dest="days", help="Number of days to download", default=35)
+    parser.add_argument("-st", "--starttime", dest="starttime_ms",
+                        help="Epoch time (milliseconds) to start from", default=1404079200000)
+    # epoch time 1404079200000 (ms) ==> 6/30/2014 Monday 12am (GMT+2)
+
     args = parser.parse_args()
 
+    # input check
+    assert isinstance(args.days, int)
+    assert isinstance(args.starttime_ms, long)
+
     number_of_days = args.days
+    starttime_ms = args.starttime_ms
     pathname = "../dbusdata/"
-    csv_filename = "dbus_proj_location_{}days.csv".format(number_of_days)
+    csv_filename = "dbus_proj_location_{starttime_s}_{days}days.csv".format(starttime_s=starttime_ms/1000, days=number_of_days)
 
     if os.path.isfile(pathname + csv_filename):
         flag_overwrite = prompt_for_yes_no("File already exists. Overwrite?")
+    else:
+        flag_overwrite = True
 
     if flag_overwrite:
-        # epoch time 1404079200000 (ms) ==> 6/30/2014 12am (GMT+2)
-        query_string = 'SELECT device_id, "time", latitude, longitude, speed, bearing, accuracy, ' \
-                       'postmile, trip_id, driver_id, bus_id, dt, delay ' \
-                       'FROM projected_location ' \
-                       'WHERE time >= 1404079200000::bigint and time < 1404079200000::bigint + {}*24*3600000::bigint ' \
-                       'ORDER BY device_id, time '.format(number_of_days)
+        query_string = "SELECT p.device_id, p.time, p.latitude, p.longitude, p.speed, p.bearing, p.accuracy, " \
+                       "p.postmile, p.trip_id, t.shape_id, r.route_short_name, p.driver_id, p.bus_id, p.dt, p.delay " \
+                       "FROM projected_location p " \
+                       "JOIN gtfs_trips_history t on t.trip_id = p.trip_id " \
+                       "JOIN gtfs_routes_history r on t.route_id = r.route_id " \
+                       "WHERE p.time >= {starttime_ms}::bigint " \
+                       "and p.time < {starttime_ms}::bigint + {days}*24*3600000::bigint " \
+                       "and r.t_range @> to_timestamp('{starttime_s}') " \
+                       "and t.t_range @> to_timestamp('{starttime_s}') " \
+                       "ORDER BY p.trip_id, p.time " \
+                       .format(starttime_ms=starttime_ms, starttime_s=starttime_ms/1000, days=number_of_days)
 
         df = query_dbus_data_postgres(args.username, args.password, query_string)
         print "Query returns {} entries of data".format(len(df))
